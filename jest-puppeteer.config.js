fe1974e6da8a1a766c7009cba6c602f2af2e12bb
Carlos Tangarife <ctangarife@rentingcolombia.com>
2020-06-01 11:23:02 -0500

    .

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     launch: { 
         headless: true,
-        args: ["--start-maximized"],
+        args: [
+            "--start-maximized",
+            "--no-sandbox",
+            "--disable-setuid-sandbox"
+            ],
         waitUntil: "networkidle2" }
 }
